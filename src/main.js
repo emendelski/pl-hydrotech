b706d8d15f8b9f98b90255d3ba97b92069243a54
Piotr Misiuk <pmisiuk@gmail.com>
2020-08-18 18:24:12 +0200

    Menu spy

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import fullpage from 'fullpage.js';
 
 // eslint-disable-next-line
 new fullpage('#fullpage', {
+  licenseKey: '891E39CB-32474BA9-909C7F6B-4B8A88C1',
   autoScrolling:true,
-  anchors:[
+  anchors: [
     'header',
     'problem',
     'solution',
     'technology',
     'about',
     'team',
-    'contact'],
-  licenseKey: '891E39CB-32474BA9-909C7F6B-4B8A88C1'
+    'contact'
+  ],
+  menu: '#menu',
 });

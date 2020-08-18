c9cf9eaa7096af716b53be864a0af952182dd0d2
Piotr Misiuk <pmisiuk@gmail.com>
2020-08-18 17:51:34 +0200

    Ids problem fixed

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import fullpage from 'fullpage.js';
 new fullpage('#fullpage', {
   autoScrolling:true,
   anchors:[
+    'header',
     'problem',
     'solution',
     'technology',
     'about',
+    'team',
     'contact'],
   licenseKey: '891E39CB-32474BA9-909C7F6B-4B8A88C1'
 });

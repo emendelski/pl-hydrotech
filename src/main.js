bd4c6b1cd1a9edb6d54a0c252f1b537b87b5017d
Piotr Misiuk <pmisiuk@gmail.com>
2020-08-25 12:50:37 +0200

    Basic job

--- src/main.js
+++ src/main.js
@@ -17,3 +17,27 @@ new fullpage('#fullpage', {
   menu: '#menu',
   responsiveHeight: 640,
 });
+
+//Modal
+
+const openItems = document.querySelectorAll('[data-modal-open]');
+const modals = document.querySelectorAll('[data-modal]');
+
+if (openItems.length) {
+
+  openItems.forEach(i => {
+    i.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = i.dataset.modalOpen;
+      const modal = [...modals].find(el => el.dataset.modal === target);
+      modal.classList.add('modal--open');
+      const closeItems = modal.querySelectorAll('[data-modal-close]');
+      closeItems.forEach(cl => {
+        cl.addEventListener('click', (ev)=>{
+          ev.preventDefault();
+          modal.classList.remove('modal--open');
+        })
+      })
+    })
+  })
+}

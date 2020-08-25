dd1f33fe1091fba6c058a3cf820a471e7c07b48c
Piotr Misiuk <pmisiuk@gmail.com>
2020-08-25 14:54:45 +0200

    Mobile nav

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './scss/main.scss';
 import fullpage from 'fullpage.js';
 
 // eslint-disable-next-line
-new fullpage('#fullpage', {
+const fp = new fullpage('#fullpage', {
   licenseKey: '891E39CB-32474BA9-909C7F6B-4B8A88C1',
   autoScrolling: true,
   anchors: [
@@ -18,6 +18,23 @@ new fullpage('#fullpage', {
   responsiveHeight: 640,
 });
 
+// Nav
+
+const navBtn = document.querySelector('[data-nav-toggle]');
+const navContent = document.querySelector('[data-nav]');
+
+if (navBtn) {
+  navBtn.addEventListener('click', e=> {
+    e.preventDefault();
+    if (navContent) {
+      navContent.classList.add('nav__list--open');
+      navContent.addEventListener('click', ()=>{
+        navContent.classList.remove('nav__list--open');
+      })
+    }
+  })
+}
+
 //Modal
 
 const openItems = document.querySelectorAll('[data-modal-open]');
@@ -31,11 +48,15 @@ if (openItems.length) {
       const target = i.dataset.modalOpen;
       const modal = [...modals].find(el => el.dataset.modal === target);
       modal.classList.add('modal--open');
+      fp.setAllowScrolling(false);
+      fp.setKeyboardScrolling(false);
       const closeItems = modal.querySelectorAll('[data-modal-close]');
       closeItems.forEach(cl => {
         cl.addEventListener('click', (ev)=>{
           ev.preventDefault();
           modal.classList.remove('modal--open');
+          fp.setAllowScrolling(true);
+          fp.setKeyboardScrolling(true);
         })
       })
     })
